d7c5cd7244351e90478a8ccf46cdfbafd659de76
devangelista2 <davide.evangelista5@unibo.it>
2023-03-14 00:03:33 +0100

    Adding support for NAFNet.

---
+++ nn/_blocks.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow import keras as ks
+
+def BaselineBlock(x, c, DW_Expand=1, FFN_Expand=2):
+    """
+    An implementation of Baseline Block from the paper: Simple Baselines for Image Restoration
+    """
+    # Define trainable constants
+    beta = tf.Variable(initial_value=0, dtype=tf.float32, trainable=True)
+    gamma = tf.Variable(initial_value=0, dtype=tf.float32, trainable=True)
+
+    # Compute the expanded channels
+    dw_channel = c * DW_Expand
+    ffn_channel = c * FFN_Expand
+
+    # Layer normalization
+    h = ks.layers.LayerNormalization()(x)
+
+    # Conv - Conv - GeLU - SE - Conv
+    h = ks.layers.Conv2D(dw_channel, kernel_size=1, strides=1, padding='same', groups=1)(h)
+    h = ks.layers.Conv2D(dw_channel, kernel_size=3, strides=1, padding='same', groups=1)(h)
+    h = ks.activations.gelu(h)
+    h = ks.layers.Multiply()([h, ChannelAttention(h, dw_channel)])
+    h = ks.layers.Conv2D(c, kernel_size=1, strides=1, padding='same', groups=1)(h)
+
+    # y = x + beta * h
+    x = x + beta * h
+
+    # Second Layer Normalization
+    h = ks.layers.LayerNormalization()(x)
+
+    # Conv - GeLU - Conv
+    h = ks.layers.Conv2D(ffn_channel, kernel_size=1, strides=1, padding='same', groups=1)(h)
+    h = ks.activations.gelu(h)
+    h = ks.layers.Conv2D(c, kernel_size=1, strides=1, padding='same', groups=1)(h)
+
+    return x + gamma * h
+
+def ChannelAttention(x, dw_channel):
+    h = ks.layers.GlobalAveragePooling2D(keepdims=True)(x)
+    h = ks.layers.Conv2D(dw_channel//2, kernel_size=1, padding='same', strides=1, groups=1)(h)
+    h = ks.layers.ReLU()(h)
+    h = ks.layers.Conv2D(dw_channel, kernel_size=1, padding='same', strides=1, groups=1)(h)
+    return ks.activations.sigmoid(h)
