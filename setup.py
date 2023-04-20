5a32471207183d25c4c6026abde312927400086a
devangelista2 <davide.evangelista5@unibo.it>
2023-04-20 12:24:30 +0200

    Updated to version 1.0.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='IPPy',
     packages=find_packages(include=['IPPy', 'IPPy.nn', 'IPPy.tomography', 'IPPy.experimental']),
-    version='0.1.0',
-    description='My first Python library',
+    version='1.0.0',
+    description='IPPy - Inverse Problems with Python.',
     author='Davide Evangelista',
     license='MIT',
-    install_requires=['numpy', 'matplotlib', 'scikit-image', 'tensorflow'],
+    install_requires=['numpy', 'matplotlib', 'scikit-image', 'tensorflow', 'tabulate'],
     setup_requires=[],
 )
